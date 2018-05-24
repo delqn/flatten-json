9c7010cfe90cac206da0888d2e031c067ef25959
Delyan Raychev <delyanr@gmail.com>
2018-05-23 23:20:04 -0700

    Better test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # flatten-json
+
+The goal if this tiny script is to take an arbitrary JSON and flatten it into a one level dict.
+
+For instance:
--- tests/test_flatten.py
+++ tests/test_flatten.py
@@ -79,6 +79,19 @@ class TestFlattenJSON(unittest.TestCase):
         }
         self.assertEqual(actual, expected)
 
+    def test_flatten_this(self):
+        simple = '{"x.y":[{"x.1":1, "n":{"a":1, "b":2}},   {"k":"l"}],"b":"c", "0":1}'
+        actual = flatten_json.flatten_json(simple)
+        expected = {
+            'b': 'c',
+            'x\\.y.0.n.a': '1',
+            'x\\.y.0.n.b': '2',
+            'x\\.y.0.x\\.1': '1',
+            'x\\.y.1.k': 'l',
+            '0': '1',
+        }
+        self.assertEqual(actual, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
