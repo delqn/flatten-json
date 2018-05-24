8e51086ba7ca5a18736cb6e443385d9ed19ae2d8
Delyan Raychev <delyanr@gmail.com>
2018-05-23 23:57:56 -0700

    Adding a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='python-flatten-json',
+    description='Flatten complex JSON into key-value format',
+    version='0.0.1',
+    license='GPLv3',
+    author='Delyan Raychev',
+    author_email='delyanr@gmail.com',
+    packages=['flatten_json'],
+)
